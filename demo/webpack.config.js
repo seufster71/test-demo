2a0e3f0602ccd936363faeef82d5f47166a498db
edward.seufert <edward.seufert@TPA1WS01190.corp.autoclubfl.com>
2017-09-19 10:29:27 -0400

    test

---
+++ demo/webpack.config.js
@@ -0,0 +1,28 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'src/main/resources/static/build');
+var APP_DIR = path.resolve(__dirname, 'src/main/js');
+
+var config = {
+  entry: APP_DIR + '/index.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+      loaders: [
+          {
+        	  test: /\.(js|jsx)$/,
+        	  include : APP_DIR,
+              loader: 'babel-loader',
+              query: {
+                  cacheDirectory: true,
+                  presets: ['es2015', 'react']
+              }
+          }
+      ]
+  }
+};
+
+module.exports = config;
